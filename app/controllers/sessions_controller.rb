f19b899844638a0780816c63338aafe857665cfe
proales <proales@proales.com>
2013-03-17 20:31:53 -0500

    updated user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,7 @@ class SessionsController < ApplicationController
     if user.present?
       if user.authenticate(params[:password])
         session[:user_id] = user.id
+        session[:id] = user.id
 
         @userdes = UserDetail.find_by_user_id(user.id)
         session[:admin] = @userdes.admin
