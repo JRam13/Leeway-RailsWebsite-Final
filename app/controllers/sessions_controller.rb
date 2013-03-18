a1aa58c2092082a031c030220ee6caf7d3e907f0
Jonny Ramos <jramos@uchicago.edu>
2013-03-17 19:43:12 -0500

    Profile 2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -26,8 +26,8 @@ class SessionsController < ApplicationController
   end
 
   def delete_account
-    @u = User.find_by_id(params[:id])
-    @u.password = "verdsfserbsdfaserdvwefsdfasdfjals;dkfjasodifwer"+Rand(100000)
+    @u = User.find_by_id(params[:user_id])
+    @u.password = "verdsfserbsdfaserdvwefsdfasdfjals;dkfjasodifwer"+Random.rand(100000).to_s
     @u.save
     reset_session
     redirect_to root_url, notice: "Successfully Deleted Account!"
