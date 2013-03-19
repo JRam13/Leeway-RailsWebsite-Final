e32ec02493d0487b9c29ea0c3759821f54ffc4cd
proales <proales@proales.com>
2013-03-19 15:15:47 -0500

    broken

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,6 @@ class UsersController < ApplicationController
   # GET /users/new.json
   def new
     @user = User.new
-
     # respond_to do |format|
     #   format.html # new.html.erb
     #   format.json { render json: @user }
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,22 @@ class User < ActiveRecord::Base
   attr_accessible :name, :password
 
   has_secure_password
-  #validates_uniqueness_of :name
+  validates_uniqueness_of :name
+  #validates :name, :uniqueness => true
 
   has_many :user_sites
   has_many :sites, :through => :user_sites
   has_many :user_details
   has_many :credit_cards
   has_many :addresses
+
+  #validate :blah
+ 
+  def blah
+    if !User.find_by_name("jeff").blank?
+      errors.add(:name, "Must be Unique!")
+    end
+  end
 
   # def create_full_user
   # 	@u = UserDetail.new
