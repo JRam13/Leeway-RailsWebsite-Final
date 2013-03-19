5e1f8ce801ee9a7eff69a0085f4312f667bf6607
proales <proales@proales.com>
2013-03-19 14:09:01 -0500

    update mailer 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,17 @@
 Leeway::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
+     :address              => "smtp.mailgun.org",
+     :port                 => 587,
+     :user_name            => "postmaster@leeway.mailgun.org",
+     :password             => "123456",
+     :authentication       => 'plain',
+     :enable_starttls_auto => true
+   }
+
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
