50761fe3b34fd8fa3f730e298027acf8c029d445
proales <proales@proales.com>
2013-03-20 16:55:52 -0500

    added callback

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -2,5 +2,29 @@ class CreditCard < ActiveRecord::Base
   attr_accessible :address_id, :ccv, :month, :number, :user_id, :year
   belongs_to :user
   belongs_to :address
-  
+
+  after_create :cleanup
+
+  def cleanup
+
+    if CreditCard.last.number.nil?
+      @ud = CreditCard.last
+      @ud.number = " "
+      @ud.save 
+    end
+
+    if CreditCard.last.ccv.nil?
+      @ud = CreditCard.last
+      @ud.ccv = 000
+      @ud.save 
+    end
+
+    if CreditCard.last.year.nil?
+      @ud = CreditCard.last
+      @ud.year = 2000
+      @ud.save 
+    end
+    
+  end
+
 end
